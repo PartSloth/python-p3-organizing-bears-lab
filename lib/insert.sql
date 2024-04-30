aca2b61518ea00d801197d916cb2599a64adfcb5
Louiza Mak <louiza.mak@gmail.com>
2024-04-30 11:21:32 -0500

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) values ('Mr. Chocolate', 2, 'M', 'black', 'aggressive', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) values ('Rowdy', 3, 'M', 'brown', 'aggressive', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) values ('Tabitha', 1, 'F', 'brown', 'docile', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) values ('Sergeant Brown', 6, 'M', 'grizzly', 'aggressive', FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) values ('Melissa', 'F', 1, 'polar', 'aggressive', FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) values ('Grinch', 'M', 5, 'black', 'docile', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) values ('Wendy', 'F', 8, 'polar', 'neutral', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) values (NULL, 'F', 2, 'grizzly', 'aggressive', TRUE);
